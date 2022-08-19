1897b28ec0c4721f50d09f9a4d5233c87984080b
Guilherme Enrique <guilherme.dantas416@gmail.com>
2022-08-19 09:05:18 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Primeira criação React Native
         </p>
         <a
           className="App-link"
