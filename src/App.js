21fa18daabad1f33ecf09224cc939168d9f24172
Guilherme Enrique <guilherme.dantas416@gmail.com>
2022-08-23 10:09:34 -0400

    useState

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import Nome from "./components/Nome";
+import { useState } from 'react'; 
 
 function App(){
+  const [nome, setNome] = useState('João P.');
+
+  function handleChangeName(nome) {
+    setNome(nome);
+  }
+
   return (
     <div>
       <h1>
         Bem vindo ao primeiro App em react
       </h1>
-      <br/>
-      <Nome nome="João Pedro" idade='34'/>
+      <h2>Olá: {nome} </h2>
+      <button onClick={ () => handleChangeName('Guilherme Enrique')}> Mudar nome </button>
     </div>
 
   );
