2429a72ef076ace17efffc45716e4536b2e03860
Guilherme Enrique <guilherme.dantas416@gmail.com>
2022-09-01 13:38:09 -0400

    criação do header

---
+++ src/pages/Error/index.js
@@ -0,0 +1,10 @@
+function Error() {
+    return (
+        <div>
+            <h2>
+                Ops, pagina não encontrada! :C
+            </h2>
+        </div>
+    )
+}
+export default Error;
--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import Home from './pages/Home';
 import Sobre from './pages/Sobre';
 import Contato from './pages/Contato';
+import Error from './pages/Error';
 
 function RoutesApp() {
     return (
         <BrowserRouter>
+            <Header />
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/sobre" element={<Sobre />} />
                 <Route path="/contato" element={<Contato />} />
+                
+                <Route path="*" element={<Error />} />
             </Routes>
         </BrowserRouter>
     )
