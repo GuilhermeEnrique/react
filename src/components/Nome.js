02d29f9fa12a2f3a9def4a91e50ef93db86b58a5
Guilherme Enrique <guilherme.dantas416@gmail.com>
2022-09-01 10:32:41 -0400

    direcionando rotas

--- src/components/Nome.js
+++
@@ -1,9 +0,0 @@
-function Nome({ nome, idade }) {
-    return (
-        <span>
-            Aqui é seu ambiente react: {nome} - Idade: {idade} anos
-        </span>
-    )
-}
-
-export default Nome;
--- src/pages/Sobre/index.js
+++ src/pages/Sobre/index.js
@@ -1,9 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function Sobre() {
     return (
         <div>
             <h1>
                 Pagina sobre a empresa.
-            </h1>
+            </h1> <br />
+            <Link to='/'>Home</Link> <br />
+            <Link to='/contato'>Contato</Link>
         </div>
     );
 }
